dd63a1a6c349bba9a66d3c317076d762ad2f2062
rong-xyz <rong@pathintegral.xyz>
2025-04-08 20:31:51 +0800

    remove resource temporary

--- servers/python-code-execution/src/python_code_execution/local_python_executor.py
+++ servers/python-code-execution/src/python_code_execution/local_python_executor.py
@@ -1418,18 +1418,18 @@ def evaluate_python_code(
     state["_operations_count"] = {"counter": 0}
 
     # only for linux
-    if sys.platform == "linux":
-        resource.setrlimit(resource.RLIMIT_CPU,
-                           (max_cpu_time_sec, max_cpu_time_sec))
-        # Increase memory limit to 500MB to accommodate numpy
-        numpy_memory_mb = 500  # Higher limit for numpy
-        memory_limit = numpy_memory_mb if "numpy" in authorized_imports else max_memory_mb
-        resource.setrlimit(resource.RLIMIT_AS, (memory_limit *
-                           1024 * 1024, memory_limit * 2 * 1024 * 1024))
-        # Prevent file creation by setting file size limit to 0
-        # resource.setrlimit(resource.RLIMIT_FSIZE, (0, 0))
-        # # Prevent file opening by setting open file limit to 0
-        # resource.setrlimit(resource.RLIMIT_NOFILE, (0, 0))
+    # if sys.platform == "linux":
+    #     resource.setrlimit(resource.RLIMIT_CPU,
+    #                        (max_cpu_time_sec, max_cpu_time_sec))
+    #     # Increase memory limit to 500MB to accommodate numpy
+    #     numpy_memory_mb = 500  # Higher limit for numpy
+    #     memory_limit = numpy_memory_mb if "numpy" in authorized_imports else max_memory_mb
+    #     resource.setrlimit(resource.RLIMIT_AS, (memory_limit *
+    #                        1024 * 1024, memory_limit * 2 * 1024 * 1024))
+    # Prevent file creation by setting file size limit to 0
+    # resource.setrlimit(resource.RLIMIT_FSIZE, (0, 0))
+    # # Prevent file opening by setting open file limit to 0
+    # resource.setrlimit(resource.RLIMIT_NOFILE, (0, 0))
 
     try:
         expression = ast.parse(code)
