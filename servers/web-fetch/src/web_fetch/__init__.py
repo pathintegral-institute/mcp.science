3769de462c0543d901f6c13315ae498963b5ecad
todd zhang <toddzhang@pathintegral.xyz>
2025-04-15 18:08:39 +0800

    web fetch with sse

--- servers/web-fetch/src/web_fetch/__init__.py
+++ servers/web-fetch/src/web_fetch/__init__.py
@@ -1,21 +1,32 @@
-import argparse
+import click
 
-from web_fetch.fetch import serve
+from web_fetch.fetch import mcp, DEFAULT_USER_AGENT
 
-DEFAULT_USER_AGENT = "ModelContextProtocol/1.0 (User-Specified; +https://github.com/modelcontextprotocol/servers)"
 
-def main():
-    parser = argparse.ArgumentParser("a general web content fetch tool")
+@click.command()
+@click.option(
+    "-t",
+    "--transport",
+    type=click.Choice(["stdio", "sse"]),
+    default="stdio",
+    help="Transport to use for requests",
+)
+@click.option(
+    "-u",
+    "--user-agent",
+    type=str,
+    default=DEFAULT_USER_AGENT,
+    help="User-Agent header to use for requests",
+)
+def main(transport: str, user_agent: str):
+    import logging
 
-    parser.add_argument(
-        "--user-agent",
-        type=str,
-        default=DEFAULT_USER_AGENT,
-        help="User-Agent header to use for requests"
+    logger = logging.getLogger(__name__)
+
+    logger.info(
+        f"Starting server with transport: {transport} and user agent: {user_agent!r}"
     )
-    args = parser.parse_args()
-
-    serve(args.user_agent)
+    mcp.run(transport)
 
 
 if __name__ == "__main__":
